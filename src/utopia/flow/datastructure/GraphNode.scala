ead474c65b213926e071e8238c44e0851d2c3dd3
Mikkomario <mikko_hilpinen@hotmail.com>
2016-10-30 11:07:12 +0200

    Finished graphNode and tested it

--- src/utopia/flow/datastructure/GraphNode.scala
+++ src/utopia/flow/datastructure/GraphNode.scala
@@ -3,13 +3,15 @@ package utopia.flow.datastructure
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.Stack
 import scala.collection.immutable.List
+import scala.collection.immutable.HashSet
 
 /**
  * Graph nodes contain content and are connected to other graph nodes via edges
  * @author Mikko Hilpinen
  * @since 28.10.2016
  */
-class GraphNode[NodeContent, EdgeContent](var content: NodeContent) extends Node[NodeContent]
+class GraphNode[NodeContent, EdgeContent](var content: NodeContent, val defaultEdgeContent: EdgeContent) 
+        extends Node[NodeContent]
 {
     // TYPES    --------------------
     
@@ -27,7 +29,7 @@ class GraphNode[NodeContent, EdgeContent](var content: NodeContent) extends Node
     /**
      * The edges that leave from this node, pointing to other nodes
      */
-    var leavingEdges = Array[Edge]()
+    var leavingEdges = HashSet[Edge]()
     
     
     // COMPUTED PROPERTIES    -------
@@ -46,7 +48,7 @@ class GraphNode[NodeContent, EdgeContent](var content: NodeContent) extends Node
      */
     def this(other: GraphNode[NodeContent, EdgeContent]) =
     {
-        this(other.content)
+        this(other.content, other.defaultEdgeContent)
         leavingEdges ++= other.leavingEdges
     }
     
@@ -54,19 +56,44 @@ class GraphNode[NodeContent, EdgeContent](var content: NodeContent) extends Node
     // OTHER METHODS    ------------
     
     /**
+     * Connects this node to another node, creating a new edge. This will always create a new edge, 
+     * even when there exists a connection already.
+     * @param node The node this node will be connected to
+     * @param edgeContent The contents for the edge that is generated (optional, default value
+     * (provided in node constructor) is used by default)
+     */
+    def connect(node: Node, edgeContent: EdgeContent = defaultEdgeContent) = leavingEdges += 
+            new GraphEdge(edgeContent, node)
+    
+    /**
+     * Removes any connection(s) to the provided node from this node. The provided node may still 
+     * contain edges towards this node afterwards.
+     * @param node The node that is disconnected from this node
+     */
+    def disconnect(node: AnyNode) = leavingEdges = leavingEdges.filterNot(edge => edge.end == node)
+    
+    /**
      * Finds an edge pointing to another node, if there is one
      * @param other The node this node may be connected to
      * @return an edge connecting the two nodes, if there is one. If there are multiple edges
-     * pointing toward the specified node, returns the first one.
+     * pointing toward the specified node, returns one of them chosen randomly.
+     * @see edgesTo(GraphNode[_, _])
      */
     def edgeTo(other: AnyNode) = leavingEdges.find(edge => edge.end == other)
+    
+    /**
+     * Finds all edges pointing from this node to the provided node.
+     * @param other another node
+     * @return The edges pointing towards the provided node from this node
+     */
+    def edgesTo(other: AnyNode) = leavingEdges.filter(edge => edge.end == other)
     
     /**
      * Finds the shortest (least amount of edges) route from this node to another node
      * @param node the node traversed to
      * @return The shortest route from this node to the provided node, if any exist
      */
-    def findShortestRouteTo(node: AnyNode) = findCheapestRouteTo(node, { edge => 1 })
+    def shortestRouteTo(node: AnyNode) = cheapestRouteTo(node, { edge => 1 })
     
     /**
      * Finds the 'cheapest' route from this node to the provided node, if there is one. A special 
@@ -76,12 +103,12 @@ class GraphNode[NodeContent, EdgeContent](var content: NodeContent) extends Node
      * contents, for example)
      * @return The cheapest route found, if any exist
      */
-    def findCheapestRouteTo(node: AnyNode, costOf: (Edge) => Double) = 
+    def cheapestRouteTo(node: AnyNode, costOf: (Edge) => Double) = 
     {
         var cheapestRoute: Option[Route] = None
         var smallestCost = 0.0
         
-        for (route <- findRoutesTo(node))
+        for (route <- routesTo(node))
         {
             val cost = route.foldLeft(0.0)((currentCost, edge) => currentCost + costOf(edge))
             if (cheapestRoute.isEmpty || cost < smallestCost)
@@ -102,14 +129,14 @@ class GraphNode[NodeContent, EdgeContent](var content: NodeContent) extends Node
      * however, a single empty route will be returned. The end node will always be at the end of
      * each route and nowhere else. If there are no connecting routes, an empty array is returned.
      */
-    def findRoutesTo(node: AnyNode): Array[Route] = findRoutesTo(node, Array[AnyNode]())
+    def routesTo(node: AnyNode): Vector[Route] = routesTo(node, Array[AnyNode]())
     
     // Uses recursion
-    private def findRoutesTo(node: AnyNode, visitedNodes: Array[AnyNode]): Array[Route] = 
+    private def routesTo(node: AnyNode, visitedNodes: Array[AnyNode]): Vector[Route] = 
     {
         if (node == this)
         {
-            Array(emptyRoute)
+            Vector(emptyRoute)
         }
         else
         {
@@ -119,11 +146,11 @@ class GraphNode[NodeContent, EdgeContent](var content: NodeContent) extends Node
     		for (edge <- leavingEdges.filterNot(edge => visitedNodes.contains(edge.end)))
     		{
     		    // Records each found route. The traversed edge is included in the returned route(s).
-    	        routes ++= edge.end.findRoutesTo(node, visitedNodes.+:(this)).map { 
+    	        routes ++= edge.end.routesTo(node, visitedNodes :+ this).map { 
     	                route => route.::(edge)}
     		}
         	
-        	routes.toArray
+        	routes.toVector
         }
     }
 }
---
+++ src/utopia/flow/test/GraphTest.scala
@@ -0,0 +1,46 @@
+package utopia.flow.test
+
+import utopia.flow.datastructure.GraphNode
+
+/**
+ * This test tests the features implemented in graph, graphNode and graphEdge
+ */
+object GraphTest
+{
+    def main(args: Array[String]): Unit = 
+	{
+        // Creates a test graph first
+        val node1 = new GraphNode(1, 1)
+        val node2 = new GraphNode(2, 1)
+        val node3 = new GraphNode(3, 1)
+        val node4 = new GraphNode(4, 1)
+        val node5 = new GraphNode(5, 1)
+        
+        // Connects the nodes (1 -> 2 -> 3 -> 5, 1 -> 4 -> 5) using weighted edges
+        node1.connect(node2)
+        node2.connect(node3)
+        node3.connect(node5)
+        
+        node1.connect(node4, 3)
+        node4.connect(node5, 3)
+        
+        // Makes sure there are correct number of edges in nodes
+        assert(node1.leavingEdges.size == 2)
+        assert(node2.leavingEdges.size == 1)
+        assert(node5.leavingEdges.isEmpty)
+        
+        // Finds the routes from 1 to 5. Should have 2 routes
+        assert(node1.routesTo(node5).size == 2)
+        
+        // The shortest route should be 1 -> 4 -> 5
+        assert(node1.shortestRouteTo(node5).get.toVector.size == 2)
+        // The cheapest route (weights considered) should be 1 -> 2 -> 3 -> 5
+        assert(node1.cheapestRouteTo(node5, edge => edge.content).get.toVector.size == 3)
+        
+        // After disconnecting node 5 from node 4. Only one route should remain
+        node4.disconnect(node5)
+        assert(node1.routesTo(node5).size == 1)
+        
+        print("Success")
+	}
+}
