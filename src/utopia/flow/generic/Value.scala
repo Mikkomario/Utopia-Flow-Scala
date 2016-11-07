b8fbe96267757f167c46995ce0d51f93a36c58e1
Mikkomario <mikko_hilpinen@hotmail.com>
2016-11-07 20:48:04 +0200

    Working on the data type casting

--- src/utopia/flow/generic/Value.scala
+++ src/utopia/flow/generic/Value.scala
@@ -1,12 +1,13 @@
 package utopia.flow.generic
 
 import utopia.flow.datastructure.template.Node
+import utopia.flow.util.Equatable
 
 /**
  * Values can wrap an object value and associate it with a certain data type. Values can be cast 
  * to different data types. They are immutable.
  */
-class Value(val content: Any, val dataType: DataType) extends Node[Any]
+class Value(val content: Any, val dataType: DataType) extends Node[Any] with Equatable
 {
     // INITIAL CODE    ---------
     
@@ -14,4 +15,12 @@ class Value(val content: Any, val dataType: DataType) extends Node[Any]
     require(dataType.isInstance(content), s"$content is not of type $dataType")
     
     
+    // COMP. PROPERTIES    -----
+    
+    override def properties = Vector(content, dataType)
+    
+    /**
+     * The description of this value, describing both content and data type
+     */
+    def description = s"$content ($dataType)"
 }
--- src/utopia/flow/test/DataTypeTest.scala
+++ src/utopia/flow/test/DataTypeTest.scala
@@ -3,6 +3,7 @@ package utopia.flow.test
 import utopia.flow.generic.DataType
 import utopia.flow.generic.AnyType
 import utopia.flow.generic.StringType
+import utopia.flow.generic.ConversionReliability
 
 object DataTypeTest extends App
 {
@@ -10,4 +11,6 @@ object DataTypeTest extends App
     
     DataType.values.foreach { println(_) }
     println("Success")
+    
+    assert(ConversionReliability.DANGEROUS < ConversionReliability.NO_CONVERSION)
 }
