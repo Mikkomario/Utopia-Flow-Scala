81f6ca1fc2f9f4189434efd45b975c14a670302d
Mikkomario <mikko_hilpinen@hotmail.com>
2016-11-29 18:13:14 +0200

    Working on the immutable model implementation

--- src/utopia/flow/datastructure/immutable/Model.scala
+++ src/utopia/flow/datastructure/immutable/Model.scala
@@ -8,16 +8,31 @@ import utopia.flow.datastructure.template
  * @author Mikko Hilpinen
  * @since 29.11.2016
  */
-class Model[Attribute <: Constant](content: Traversable[Attribute], 
-        attributeFactory: (String) => Option[Attribute]) extends template.Model[Attribute]
+class Model[Attribute <: Constant](val attributeFactory: (String) => Option[Attribute], 
+        content: Traversable[Attribute]) extends template.Model[Attribute]
 {
-    // ATTRIBUTES    ------------
+    // ATTRIBUTES    --------------
     
     // Filters out duplicates (case-insensitive)
     val attributes = content.groupBy { _.name.toLowerCase() }.values.map { _.head }.toSet
     
     
-    // IMPLEMENTED METHODS    --
+    // CONSTRUCTOR OVERLOAD    ---
+    
+    def this(attributeFactory: (String) => Option[Attribute], content: Attribute*) = 
+            this(attributeFactory, content)
+    
+    
+    // IMPLEMENTED METHODS    ----
     
     override def generateAttribute(attName: String) = attributeFactory(attName)
+    
+    
+    // OPERATORS    --------------
+    
+    def +(attribute: Attribute) = new Model(attributeFactory, attributes + attribute)
+    
+    def ++(attributes: Traversable[Attribute]) = new Model(attributeFactory, this.attributes ++ attributes)
+    
+    def ++(other: Model[Attribute]) = new Model(attributeFactory, attributes ++ other.attributes)
 }
