a48ea3b6bee073d2859296b8b4c9e1dc9ac976ff
Mikkomario <mikko_hilpinen@hotmail.com>
2018-11-17 18:33:44 +0200

    Added an extension to the traditional comparable class

---
+++ src/utopia/flow/util/RichComparable.scala
@@ -0,0 +1,60 @@
+package utopia.flow.util
+
+import scala.annotation.unchecked.uncheckedVariance
+
+object RichComparable
+{
+    // Extension for existing comparables
+    implicit class ExtendedComparable[T](val c: Comparable[T]) extends RichComparable[T]
+    {
+        def compareTo(other: T) = c.compareTo(other)
+    }
+    
+    /**
+     * Finds the smaller of the two provided values
+     */
+    def min[B, A <: Comparable[B]](a: A, b: B) = if (a.isLargerThan(b)) b else a
+    
+    /**
+     * Finds the larger of the two values
+     */
+    def max[B, A <: Comparable[B]](a: A, b: B) = if (a.isSmallerThan(b)) b else a
+}
+
+/**
+* This is an extension trait for the comparable interface in Java
+* @author Mikko Hilpinen
+* @since 17.11.2018
+**/
+trait RichComparable[-T] extends Comparable[T @uncheckedVariance]
+{
+    /**
+     * Checks whether this item is larger than the specified item
+     */
+	def isLargerThan(other: T) = compareTo(other) > 0
+	
+	/**
+	 * Checks whether this item is smaller than the specified item
+	 */
+	def isSmallerThan(other: T) = compareTo(other) < 0
+	
+	/**
+	 * Checks whether this item may be considered equal with the specified item
+	 */
+	def isEqualTo(other: T) = compareTo(other) == 0
+	
+	/**
+	 * Compares this item with another, but if they are equal, provides a backup result
+	 * @param other another instance
+	 * @param backUp a function for providing a backup result in case the first one doesn't 
+	 * separate the values
+	 */
+	def compareOr(other: T)(backUp: => Int) = 
+	{
+	    val primary = compareTo(other)
+	    if (primary == 0)
+	        backUp
+	    else
+	        primary
+	}
+}
