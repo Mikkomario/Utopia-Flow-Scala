605a2de05bffc9a29fea4123d5bb8777a83f9179
Mikkomario <mikko_hilpinen@hotmail.com>
2019-05-08 14:26:14 +0300

    Moved flow tests under separate source. Added AllTest

--- src/utopia/flow/test/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains test classes used when developing the library
- */
-/**
- * @author Mikko Hilpinen
- * @since 28.10.2016
- */
-package utopia.flow.test;
---
+++ test/utopia/flow/test/AllTests.scala
@@ -0,0 +1,33 @@
+package utopia.flow.test
+
+/**
+  * This test runs all of the other tests
+  * @author Mikko Hilpinen
+  * @since 8.5.2019, v1+
+  */
+object AllTests extends App
+{
+	def run(test: App) =
+	{
+		println(s"\nRunning ${test.getClass.getName}	------------------------------")
+		test.main(Array())
+	}
+	
+	run(DataTypeTest)
+	run(CollectionTest)
+	run(CounterTest)
+	run(TreeNodeTest)
+	run(GraphTest)
+	run(ValueAccessorTest)
+	run(ModelDeclarationTest)
+	run(ModelTest)
+	run(ModelConvertibleTest)
+	run(JSONTest)
+	run(XmlTest)
+	run(VolatileTest)
+	run(WeakListTest)
+	run(AsyncTest)
+	run(LoopTest)
+	
+	println("All test completed")
+}
